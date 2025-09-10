f16e566b274b4e1bf699f8889a56029850522dfb
PanzTewsZ <patcharapon.bo@ku.th>
2025-09-10 23:29:50 +0700

    Add files via upload

--- ObserverPattern/OrderProcessor.java
+++ ObserverPattern/OrderProcessor.java
@@ -9,7 +9,17 @@ public class OrderProcessor {
       observers.add(observer);
    }
    
-   public void unregister(OrderObserver observer) {
+   public void unregister(OrderObserver observer){
       observers.remove(observer);
+    }
+    private void notifyObservers(Order order){
+      for(OrderObserver observer : observers){
+         observer.update(order);
+      }
+    }
+    public void processOrder(Order order){
+      System.out.println("\nProcessing order " + order.orderId());
+      System.out.println("Order processed successfully.");
+      notifyObservers(order);
    }
 }
